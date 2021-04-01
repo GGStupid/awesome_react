f8880250058934c3cc444a7cde39ef4854551872
Bob_lv <bob_lv@southbaytech.co>
2021-04-01 17:15:21 +0800

    feat:初始化目录解构，增加模块alias

--- script/webpack.config.js
+++ script/webpack.config.js
@@ -51,6 +51,14 @@ module.exports = {
   resolve: {
     alias: {
       "@src": path.resolve(__dirname, "../src"),
+      "@assets": path.resolve(__dirname, "../src/assets"),
+      "@components": path.resolve(__dirname, "../src/components"),
+      "@middlewares": path.resolve(__dirname, "../src/middlewares"),
+      "@pages": path.resolve(__dirname, "../src/pages"),
+      "@router": path.resolve(__dirname, "../src/router"),
+      "@services": path.resolve(__dirname, "../src/services"),
+      "@store": path.resolve(__dirname, "../src/store"),
+      "@utils": path.resolve(__dirname, "../src/utils"),
     },
     extensions: [".tsx", ".ts", ".js", ".json"],
   },
