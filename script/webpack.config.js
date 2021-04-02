948e899f4bda049135b13e5cf8de8ff1e2d28d46
bob_lv <bob_lv@admindeiMac-2.local>
2021-04-02 17:27:28 +0800

    test:test .ssh/config配置

--- script/webpack.config.js
+++ script/webpack.config.js
@@ -63,9 +63,7 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js", ".json"],
   },
   devServer: {
-    host: "0.0.0.0",
     port: 8080,
-    https: true,
     inline: true,
     stats: "errors-only",
     contentBase: path.resolve(__dirname, "dist"),
