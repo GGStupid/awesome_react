d417b2a367e661cf8c72e004c1bd7d67730babad
GGStupid <610383057@qq.com>
2021-04-27 16:48:27 +0800

    feat:修复热更新

--- script/webpack.common.js
+++ script/webpack.common.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  target: "web",
   entry: "./src/index.tsx",
   output: {
     filename: "[name].bundle.js",
