991ef932b49d1e4f70ac9301fa2f065b605b46c6
GGStupid <610383057@qq.com>
2021-05-17 17:47:04 +0800

    feat:添加transform-class-properties，重构httpclient

--- script/webpack.dev.js
+++ script/webpack.dev.js
@@ -4,7 +4,7 @@ const common = require("./webpack.common");
 
 module.exports = merge(common, {
   mode: "development",
-  devtool: "inline-source-map",
+  devtool: "eval-cheap-module-source-map",
   devServer: {
     port: 3000,
     stats: "errors-only",
--- src/common/httpClient.ts
+++ src/common/httpClient.ts
@@ -4,123 +4,103 @@ import axios, {
   AxiosResponse,
   CancelTokenStatic,
 } from "axios";
-
-// class HttpClient {
-//   baseUrl = process.env.BASE_URL || "/api/";
-//   CancelToken: CancelTokenStatic = axios.CancelToken;
-//   axiosRequestConfig: AxiosRequestConfig = {};
-//   successCode: number[] = [200, 201, 204];
-//   axiosInstance: AxiosInstance;
-
-//   constructor() {
-//     this.axiosInstance = axios.create({
-//       baseURL: this.baseUrl,
-//       timeout: 3000,
-//       headers: {
-//         timestamp: new Date().getTime(),
-//       },
-//       withCredentials: true,
-//     });
-//     this.interceptorsRequest();
-//     this.interceptorsResponse();
-//   }
-
-//   private interceptorsRequest() {
-//     if (this.axiosInstance) {
-//       this.axiosInstance.interceptors.request.use(
-//         function (config: AxiosRequestConfig) {
-//           console.log(config);
-//           config.headers["X-token"] = "adsf";
-//           return config;
-//         },
-//         function (error) {
-//           return Promise.reject(error);
-//         },
-//       );
-//     }
-//   }
-//   private interceptorsResponse() {}
-
-//   private responseLog(response: AxiosResponse) {
-//     if (process.env.NODE_ENV === "development") {
-//       const randomColor = `rgba(${Math.round(Math.random() * 255)},${Math.round(
-//         Math.random() * 255,
-//       )},${Math.round(Math.random() * 255)})`;
-//       console.log(
-//         "%c┍------------------------------------------------------------------┑",
-//         `color:${randomColor};`,
-//       );
-//       console.log("| 请求地址：", response.config.url);
-//       console.log("| 请求参数：", JSON.parse(response.config.data));
-//       console.log("| 返回数据：", response.data);
-//       console.log(
-//         "%c┕------------------------------------------------------------------┙",
-//         `color:${randomColor};`,
-//       );
-//     }
-//   }
-
-//   delete<T>(url: string, data?: any): Promise<IData<T>> {
-//     return axiosInstance.delete(url, { params: data });
-//   }
-//   put<T>(url: string, data?: any): Promise<IData<T>> {
-//     return axiosInstance.put(url, data);
-//   }
-//   get<T>(url: string, data?: any): Promise<IData<T>> {
-//     return axiosInstance.get(url, { params: data });
-//   }
-//   post<T>(url: string, data?: any): Promise<IData<T>> {
-//     return axiosInstance.post(url, data);
-//   }
-// }
-
-const baseURL = "/api/";
-
-const axiosInstance = axios.create({
-  baseURL,
-  timeout: 3000,
-});
-
-//添加请求拦截器
-axiosInstance.interceptors.request.use(
-  function (config: AxiosRequestConfig) {
-    console.log(config);
-    config.headers["X-token"] = "adsf";
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  },
-);
-
-//添加响应拦截器
-axiosInstance.interceptors.response.use(
-  function (response: AxiosRequestConfig) {
-    return response.data;
-  },
-  function (error) {
-    return Promise.reject(error);
-  },
-);
+import Toast from "@src/components/Toast";
 
 interface IData<T> {
   success: boolean;
   data: T;
 }
+class HttpClient {
+  baseUrl = "/api/";
+  CancelToken: CancelTokenStatic = axios.CancelToken;
+  axiosRequestConfig: AxiosRequestConfig = {};
+  successCode: number[] = [200, 201, 204];
+  axiosInstance: AxiosInstance;
 
-class httpClient {
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: this.baseUrl,
+      timeout: 3000,
+      headers: {
+        timestamp: new Date().getTime(),
+      },
+      withCredentials: true,
+    });
+    this.interceptorsRequest();
+    this.interceptorsResponse();
+  }
+
+  private interceptorsRequest() {
+    if (this.axiosInstance) {
+      this.axiosInstance.interceptors.request.use(
+        (config: AxiosRequestConfig) => {
+          return config;
+        },
+        (error) => {
+          return Promise.reject(error);
+        },
+      );
+    }
+  }
+  private interceptorsResponse() {
+    if (this.axiosInstance) {
+      this.axiosInstance.interceptors.response.use(
+        (response: AxiosResponse) => {
+          this.responseLog(response);
+
+          if (!this.successCode.includes(response.status)) {
+            throw response;
+          }
+
+          if (!response.data.success) {
+            console.log(!response.data.success, response.data);
+            Toast.warning(response.data.message);
+            // Promise.reject(response.data);
+            throw new Error(response.data.message);
+          }
+
+          return response.data;
+        },
+        (error) => {
+          console.log(">>>>>>", error);
+          Toast.error(error.message || "出错了，请稍后再试");
+          return Promise.reject(error);
+        },
+      );
+    }
+  }
+
+  private responseLog(response: AxiosResponse) {
+    if (process.env.NODE_ENV === "development") {
+      const randomColor = `rgba(${Math.round(Math.random() * 255)},${Math.round(
+        Math.random() * 255,
+      )},${Math.round(Math.random() * 255)})`;
+      console.log(
+        "%c┍------------------------------------------------------------------┑",
+        `color:${randomColor};`,
+      );
+      console.log("| 请求地址：", response.config.url);
+      console.log("| 请求方式：", response.config.method);
+      console.log("| 返回数据：", response.data);
+      console.log(
+        "%c┕------------------------------------------------------------------┙",
+        `color:${randomColor};`,
+      );
+    }
+  }
+
   delete<T>(url: string, data?: any): Promise<IData<T>> {
-    return axiosInstance.delete(url, { params: data });
+    return this.axiosInstance.delete(url, { params: data });
   }
   put<T>(url: string, data?: any): Promise<IData<T>> {
-    return axiosInstance.put(url, data);
+    return this.axiosInstance.put(url, data);
   }
   get<T>(url: string, data?: any): Promise<IData<T>> {
-    return axiosInstance.get(url, { params: data });
+    return this.axiosInstance.get(url, { params: data });
   }
   post<T>(url: string, data?: any): Promise<IData<T>> {
-    return axiosInstance.post(url, data);
+    return this.axiosInstance.post(url, data);
   }
 }
 
-export default new httpClient();
+export default new HttpClient();
